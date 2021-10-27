f5e320bdae8802d342f83f06e261e1ead8759d01
Akanaboina Edukondalu <ea21027@corp.ojas-it.com>
2021-10-27 10:23:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,130 @@
+/*import axios from "axios";
+import React from "react";
+
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+}*/
+
+/*import axios from "axios";
+import React from "react";
+
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(`${baseURL}/1`).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  if (!post) return "No post!"
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
+    </div>
+  );
+}*/
+
+/*import axios from "axios";
+import React from "react";
+
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(`${baseURL}/1`).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function updatePost() {
+    axios
+      .put(`${baseURL}/1`, {
+        title: "Hello World!",
+        body: "This is an updated post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  if (!post) return "No post!"
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={updatePost}>Update Post</button>
+    </div>
+  );
+}*/
+
+import axios from "axios";
+import React from "react";
+
+const client = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com/posts" 
+});
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    async function getPost() {
+      const response = await client.get("/1");
+      setPost(response.data);
+    }
+    getPost();
+  }, []);
+
+  async function deletePost() {
+    await client.delete("/1");
+    alert("Post deleted!");
+    setPost(null);
+  }
+
+  if (!post) return "No post!"
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={deletePost}>Delete Post</button>
+    </div>
+  );
+}
